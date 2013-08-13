754f65e6b91b0355ff6e4c1628538f51f2f246e9
Ionel Cristian Mărieș <contact@ionelmc.ro>
2013-08-14 01:09:17 +0300

    Changed set*interval logic to work on Python3 too.

--- src/manhole.py
+++ src/manhole.py
@@ -23,6 +23,13 @@ try:
     string = basestring
 except NameError: # python 3
     string = str
+
+if hasattr(sys, 'setswitchinterval'):
+    setinterval = sys.setswitchinterval
+    getinterval = sys.getswitchinterval
+else:
+    setinterval = sys.setcheckinterval
+    getinterval = sys.getcheckinterval
 PY3 = sys.version_info[0] == 3
 VERBOSE = True
 
@@ -151,6 +158,7 @@ class ManholeConnection(threading.Thread):
         client.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 0)
         client.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 0)
         backup = []
+        old_interval = getinterval()
         try:
             client_fd = client.fileno()
             for mode, names in (
@@ -172,22 +180,25 @@ class ManholeConnection(threading.Thread):
             run_repl()
             cry("DONE.")
         finally:
-            cry("Cleaning up.")
-            old_interval = sys.getcheckinterval()
-            sys.setcheckinterval(2147483647)
-            junk = [] # keep the old file objects alive for a bit
-            for name, fh in backup:
-                junk.append(getattr(sys, name))
-                setattr(sys, name, fh)
-            del backup
-            for fh in junk:
-                try:
-                    fh.close()
-                except IOError:
-                    pass
-                del fh
-            del junk
-            sys.setcheckinterval(old_interval)
+            try:
+                setinterval(2147483647) # change the switch/check interval to something ridiculous
+                                        # we don't want to have other thread try to write to the
+                                        # redirected sys.__std*/sys.std* - it would fail horribly
+                junk = [] # keep the old file objects alive for a bit
+                for name, fh in backup:
+                    junk.append(getattr(sys, name))
+                    setattr(sys, name, fh)
+                del backup
+                for fh in junk:
+                    try:
+                        fh.close()
+                    except IOError:
+                        pass
+                    del fh
+                del junk
+            finally:
+                setinterval(old_interval)
+                cry("Cleaned up.")
 
 def run_repl():
     dump_stacktraces()
