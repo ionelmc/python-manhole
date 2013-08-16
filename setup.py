51b4732d1073f75f36a4953b04e56f439e02ca7f
Ionel Cristian Mărieș <contact@ionelmc.ro>
2013-08-17 02:51:36 +0300

    Up version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 setup(
     name = "manhole",
-    version = "0.4.0",
+    version = "0.5.0",
     url = 'https://github.com/ionelmc/python-manhole',
     download_url = '',
     license = 'BSD',
