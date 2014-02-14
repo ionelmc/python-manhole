e99990554a3d0bad302f565777cf2283ae1e3627
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-02-14 03:11:53 +0200

    Update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 setup(
     name = "manhole",
-    version = "0.5.2",
+    version = "0.6.0",
     url = 'https://github.com/ionelmc/python-manhole',
     download_url = '',
     license = 'BSD',
